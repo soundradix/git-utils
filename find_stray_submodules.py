696d0d9d69e3a7ea24dcd84a7d9aab722c8ea811
Yair Chuchem <yairchu@gmail.com>
2015-12-20 13:35:29 +0200

    find_stary_submodules: fix for deleting stray nested submodules and add help

--- find_stray_submodules.py
+++ find_stray_submodules.py
@@ -4,6 +4,9 @@
 '''
 Finds remains of old submodules in .git
 Useful to clean up .git
+
+Usage: find_stray_submodules.py <command>
+Commands: info, list, delete
 '''
 
 import os
@@ -11,12 +14,18 @@ from pathlib import Path
 import shutil
 import sys
 
-if len(sys.argv) > 1:
-    assert len(sys.argv) == 2
-    mode = sys.argv[1]
-    assert mode in ['info', 'list', 'delete']
-else:
+def show_help():
+    print(__doc__)
+    sys.exit(1)
+
+modes = ['info', 'list', 'delete']
+
+if len(sys.argv) == 1:
     mode = 'info'
+elif len(sys.argv) > 2 or sys.argv[1] not in modes:
+    show_help()
+else:
+    mode = sys.argv[1]
 
 def try_remove_prefix(s, prefix):
     'Removes prefix or returns None'
@@ -24,7 +33,7 @@ def try_remove_prefix(s, prefix):
         return None
     return s[len(prefix):]
 
-dirs_to_delete = []
+dirs_found = []
 for config_path in Path('.git').glob('modules/**/config'):
     module_gitdir = config_path.parent
     for config_line in config_path.open('r').readlines():
@@ -48,10 +57,12 @@ for config_path in Path('.git').glob('modules/**/config'):
             print('%s\n    references worktree which does not reference it back' % module_gitdir)
     if mode == 'list':
         print(module_gitdir)
-    if mode == 'delete':
-        dirs_to_delete.append(module_gitdir)
+    dirs_found.append(module_gitdir)
 
-for dirname in dirs_to_delete:
-    assert mode == 'delete'
-    print('deleting %s' % dirname)
-    shutil.rmtree(str(dirname))
+if mode == 'delete':
+    for dirname in dirs_found[::-1]:
+        print('deleting %s' % dirname)
+        shutil.rmtree(str(dirname))
+    print('Deleted %d stray submodules' % len(dirs_found))
+if mode == 'info' and not dirs_found:
+    print('No stary submodules found')
