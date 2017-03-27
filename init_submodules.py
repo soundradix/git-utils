7fc78a655a5aebee031de0b42e801a97f6b0efe1
Yair Chuchem <yairchu@gmail.com>
2017-03-27 18:57:15 +0300

    init_submodules.py also supports several root repos

--- init_submodules.py
+++ init_submodules.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 '''
-init_submodules.py
+init_submodules.py [paths]
 
 A tool to init a git repo of submodules with shared submodules.
 For example a repo with structure:
@@ -19,6 +19,23 @@ For example a repo with structure:
 
 Will initialized the submodules recursively so that the shared module "Lib" will only be fetched ones and be initialized using the "reference" flag.
 
+For separate repos with common submodules, one may provide paths in the command line.
+So if your repos have this structure
+
++ Root A
+|
++-+ Module Lib
+
++ Root B
+|
++-+ Module Lib
+
+Providing the paths for "Root A" and "Root B" will initialize the submodules so that
+"Root B"'s submodule references "Root A"'s submodule.
+Note that the order is important and modules in paths provided first will not reference
+modules from the paths after them.
+This is useful as some repos could be temporary and one would want the temporary repo's
+submodule to reference the permanent repo's and not the other way.
 '''
 
 import collections
@@ -44,16 +61,19 @@ def list_submodules():
             'remote': strip_end(value, '.git'),
             }
 
-class ChangeDir:
-    def __init__(self, target):
-        self.target = target
-    def __enter__(self):
-        self.prev_dir = os.getcwd()
-        os.chdir(self.target)
-    def __exit__(self, type, value, traceback):
-        os.chdir(self.prev_dir)
-
 submodule_sources = {}
+
+def init_submodule(sub):
+    source = submodule_sources.get(sub['remote'])
+    if source:
+        print('Using source %s for submodule %s' % (source, sub['local']))
+        subprocess.check_call(['git', 'submodule', 'update', '--reference', source, '--init', sub['local']])
+    else:
+        print('Initializing new submodule %s' % (sub['local'], ))
+        subprocess.check_call(['git', 'submodule', 'update', '--init', sub['local']])
+    abs_dir = os.path.abspath(sub['local'])
+    if sub['remote'] not in submodule_sources:
+        submodule_sources[sub['remote']] = abs_dir
 
 # Python 2 compatibility
 try:
@@ -61,29 +81,28 @@ try:
 except NameError:
     pass
 
-todos = collections.deque([os.getcwd()])
-while todos:
-    cur = todos.popleft()
-    print('Initializing submodules at %s' % (cur, ))
-    os.chdir(cur)
-    subs = list(list_submodules())
-    for sub in subs:
-        source = submodule_sources.get(sub['remote'])
-        if source:
-            print('Using source %s for submodule %s' % (source, sub['local']))
-            subprocess.check_call(['git', 'submodule', 'update', '--reference', source, '--init', sub['local']])
-        else:
-            print('Initializing new submodule %s' % (sub['local'], ))
+def go(path):
+    todos = collections.deque([path])
+    while todos:
+        cur = todos.popleft()
+        print('Initializing submodules at %s' % (cur, ))
+        os.chdir(cur)
+        subs = list(list_submodules())
+        for sub in subs:
             try:
-                subprocess.check_call(['git', 'submodule', 'update', '--init', sub['local']])
+                init_submodule(sub)
             except:
                 print('Failed initializing submodule %s' % (sub['local'], ))
                 sys.stdout.write('Continue (y/n)? ')
                 if not input().lower().startswith('y'):
                     sys.exit(1)
-                continue
-        with ChangeDir(sub['local']):
-            cur_dir = os.getcwd()
-            if sub['remote'] not in submodule_sources:
-                submodule_sources[sub['remote']] = os.getcwd()
-            todos.append(cur_dir)
+            else:
+                todos.append(os.path.abspath(sub['local']))
+
+if len(sys.argv) < 2:
+    paths = [os.getcwd()]
+else:
+    paths = [os.path.abspath(x) for x in sys.argv[1:]]
+
+for path in paths:
+    go(path)
